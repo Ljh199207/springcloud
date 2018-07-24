c0e6e4c016ade85548dbcfdb2a680b7737c25550
liangjinghai1 <liangjinghai@wkhgs.com>
2018-07-24 14:58:46 +0800

    1

--- service-feign/src/main/java/com/ljh/service/SchedualServiceHi.java
+++ service-feign/src/main/java/com/ljh/service/SchedualServiceHi.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 @Component
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
 
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
